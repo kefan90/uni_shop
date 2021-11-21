52875ba32878c2172c0a6bd70aaf12c32cc0c79f
kefan90 <61527573+kefan90@users.noreply.github.com>
2021-11-21 20:44:21 +0800

    init project

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<h1>首页</h1>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
